0df4bc16ac7004acb0f772055991af01042c0be6
Laurie Fish <iamlauriefish@gmail.com>
2023-05-01 18:52:13 -0700

    updated functions

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const connection = require('./config/connection');
 const inquirer = require('inquirer');
-const express = require('express');
-const { viewRoles, viewEmployees, viewDepartments } = require('./lib/view');
+
+// const { viewRoles, viewEmployees, viewDepartments } = require('./lib/view');
 const { addRole, addEmployee, addDepartment } = require('./lib/add');
 const { deleteEmployee, deleteRole, deleteDepartment } = require('./lib/delete');
-const { updateEmployee, updateDepartment, updateRole } = require('./lib/update');
+// const { updateEmployee, updateDepartment, updateRole } = require('./lib/update');
 const db = connection;
 
 
 // const PORT = process.env.PORT || 3006;
 
-const app = express();
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// init();
 
-const init = () => {
+
+
+function init() {
     inquirer.prompt([
         {
             type: 'list',
@@ -34,7 +32,7 @@ const init = () => {
                 'Add a role',
                 'Add an employee',
                 'Update an employee',
-                'Update an employee role',
+                'Update a role',
                 'Update an employee manager',
                 'Delete a department',
                 'Delete an employee',
@@ -48,42 +46,55 @@ const init = () => {
 
             if (choices === 'View department') {
                 viewDepartments();
+
             } else if
                 (choices === 'View employees') {
                 viewEmployees();
+
             }
             else if (choices === 'View roles') {
                 viewRoles();
+
             }
             else if (choices === 'Add a department') {
                 addDepartment();
+
             }
             else if (choices === 'Add an employee') {
                 addEmployee();
+
             }
             else if (choices === 'Add a role') {
                 addRole();
+
             }
             else if (choices === 'Update a department') {
                 updateDepartment();
+
             }
             else if (choices === 'Update an employee') {
                 updateEmployee();
+
             }
             else if (choices === 'Update a role') {
                 updateRole();
+
             }
             else if (choices === 'Delete a department') {
                 deleteDepartment();
+
             }
             else if (choices === 'Delete an employee') {
                 deleteEmployee();
+
             }
             else if (choices === 'Delete e a role') {
                 deleteRole();
+
             }
             else if (choices === 'See department budget') {
                 seeBudget();
+
             }
             else if (choices === 'I am finished') {
                 connection.end();
@@ -123,6 +134,110 @@ function chooseManager() {
     })
     return managerArray;
 }
+function viewDepartments() {
+    db.query('SELECT * FROM department', function (err, result) {
+        console.table(result);
+        init();
+    })
+
+}
+function viewRoles() {
+    db.query('SELECT * FROM roles', function (err, result) {
+        consoleTable(result);
+        init();
+    })
+}
+function viewEmployees() {
+    db.query('SELECT * FROM employees', function (err, result) {
+        console.table(result);
+        init();
+    })
+}
+function updateEmployee() {
+    let employeeArray = [];
+    connection.query('SELECT * FROM employees', function (err, res) {
+        if (err) throw err
+        for (var i = 0; i < res.length; i++) {
+            employeeArray.push(res[i].last_name);
+        }
+        inquirer.prompt([
+            {
+                name: 'employee',
+                type: 'list',
+                message: 'Which employee do you want to update?',
+                choices: employeeArray
+            },
+            {
+                name: 'newRole',
+                type: 'input',
+                message: 'What is the new role ID for this employee?',
+                choices: chooseRole()
+            }
+        ]).then((answers) => {
+            let employeeId = employeeArray.indexOf(answers.employee) + 1
+            connection.query('UPDATE employees SET ? WHERE ?',
+                [
+                    {
+                        role_id: answers.newRole
+                    },
+                    {
+                        id: employeeId
+                    }
+                ],
+                function (err) {
+                    if (err) throw err
+                    console.log('Employee updated');
+                    startQuestions();
+                }
+            );
+        });
+    });
+}
+
+function updateRole() {
+    let roleArray = [];
+    connection.query('SELECT * FROM roles', function (err, res) {
+        if (err) throw err
+        for (var i = 0; i < res.length; i++) {
+            roleArray.push(res[i].last_name);
+        }
+        inquirer.prompt([
+            {
+                name: 'employee',
+                type: 'list',
+                message: 'Which employee do you want to update?',
+                choices: employeeArray
+            },
+            {
+                name: 'newRole',
+                type: 'input',
+                message: 'What is the new role ID for this employee?',
+                choices: chooseRole()
+            }
+        ]).then((answers) => {
+            let roleId = roleArray.indexOf(answers.newRole) + 1
+            connection.query('UPDATE employees SET ? WHERE ?',
+                [
+                    {
+                        role_id: answers.newRole
+                    },
+                    {
+                        id: employeeId
+                    }
+                ])
+        }),
+            function (err) {
+                if (err) throw err
+                console.log('Role updated');
+                init();
+            }
+
+
+    })
+}
+
+
+
 init();
 
 
